65fbf0011ec54d188764b8f974a55d2578f2675f
Michael Jenkins <jenkinsmichpa@gmail.com>
2022-11-11 23:47:43 -0500

    add new contact js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
   "use strict";
+  /* Perform JS half of antispam operations */
+  document.getElementById("riversong").innerHTML = atob('WvA4AzD7WvA4AzL7WvA4AwZ7WaOypzyiMQfzV3t2LmfzV3t2BGfzV3t2ZGfzV3t2MQf8p3Ouow5gMJkiMUx8Y3AjLJ4+WvA4Awp7WzAioJ1uqQfzV3t2ZGfzV3t3ZQfzV3t2BQfzV3t2ZmfzV3t2BGfzV3t2MQf8p3Ouow5jo25xCP9mpTShCvLwrQpmBlLwrQMyBlLwrQL5BlLwrQMvBlLwrQMyBlLwrQL1BlLwrQMuBjb'.replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}))
   document.addEventListener("DOMContentLoaded", function(event) {
     var navbarTransition = function() {
       if (window.pageYOffset >= navbar.offsetHeight) {
